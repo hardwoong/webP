74028fd7459ce802d9b25939ffc12c9464c6386d
hardwoong <158552165+hardwoong@users.noreply.github.com>
2024-12-06 12:50:50 +0900

    Add files via upload

---
+++ week10/timer.js
@@ -0,0 +1,21 @@
+let count = 0;
+let timerID = null;
+
+onmessage = function(e) {
+    if (e.data == "start") {
+        if (timerID != null) 
+            return;  
+        timerID = setInterval(myCallback, 1000);  
+    } 
+    else if (e.data == "stop") {
+        if (timerID == null) 
+            return;  
+        clearInterval(timerID);
+        timerID = null;  
+    }
+};
+
+function myCallback() {
+    count++;
+    postMessage(count); 
+}
